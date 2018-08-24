e7f3a263da8648cb396449ee5eefe72a71712add
redassser <40395425+redassser@users.noreply.github.com>
2018-08-24 12:03:46 -0400

    bot spam now makes sense maybe

--- index.js
+++ index.js
@@ -261,6 +261,113 @@ resetBot(message.channel);
      message.channel.send(`http://www.scp-wiki.net/scp-${scp}`);
      }
   }
+   if (command === "hug") {
+    var mention = message.mentions.members.first();
+    if (message.isMentioned(mention)) {
+    message.channel.send(`(>^_^)> ${mention} <(^.^<)`)
+  }
+    else 
+      message.channel.send(`(>^_^)> ${message.author} <(^.^<)`)
+  }
+  if (command === "donate") {
+      message.channel.send("Donate here: https://www.paypal.com/pools/c/839i9RcUvF")
+}
+  if (command === "purge" && !isNaN(args[0])) {
+
+    if (message.member.permissions.has('VIEW_AUDIT_LOG')) {
+    var numb = args[0]
+    message.channel.bulkDelete(numb);
+        message.channel.send(`${numb} messages deleted!`)
+  .then(msg => {
+    msg.delete(5000);
+  })
+  .catch();
+    }
+    else
+      message.channel.send("Nope.");
+  }
+ if (command === "support") {
+   message.channel.send("No worries, a staff member will be with you soon!")
+  client.channels.get("442726774909042689").send(`<@&432338355683065856>, ${message.author} is having a bit of a problem in ${message.channel}`);
+  }
+  if (command === "commands") {
+    message.channel.send("You have been messaged the commands!")
+    message.author.send({"embed": {
+    "color": 3947583,
+    "title": "Use prefix '!'",
+        fields: [
+          {
+          name: "scp-[Any number between 1 and 3999](-j opional)",
+          value: "Links to that SCP's page."
+        },
+                 {
+          name: "hug",
+          value: "Gives a hug to you or the person you mention!"
+        },
+                 {
+          name: "donate",
+          value: "Links to the official Kurva PayPal page!"
+        },
+                 {
+          name: "commands",
+          value: "You *just* did it."
+        },
+                 {
+          name: "support",
+          value: "Tells a staff member that you need help. Do not abuse it or there will be consequences."
+        },
+          {
+          name: "kgmeme",
+          value: "Give you a free Kurva Gaming certified meme."
+        },
+         {
+          name: "ss0, 1, 2, 3, k",
+          value: "Tells you the current status of the server."
+        },
+        {
+          name: "8ball",
+          value: "Answers your question *very* truthfully."
+        },
+         {
+          name: "cah",
+          value: "Continues your sentence with a white card from Cards Against Humanity!"
+        },
+                 {
+          name: "purge\nADMIN ONLY",
+          value: "Deletes specified number of messages."
+        },
+         {
+          name: "status\nADMIN ONLY",
+          value: "Sets the **Playing** message for the bot."
+        },
+         {
+          name: "ping\nOWNER ONLY",
+          value: "Pings a person the given number of times.\nUp to 5 reccomended."
+        },
+        {
+          name: "restart\nMOD ONLY",
+          value: "Restarts the bot."
+        },
+         {
+          name: "vote\nADMIN ONLY",
+          value: "Starts a vote!"
+        },
+        {
+          name: "cc (command) (text)\nADMIN ONLY",
+          value: "The bot will say the (text) when you do !(command)."
+        },
+        {
+          name: "ccdel (command)\nADMIN ONLY",
+          value: "The command will be deleted from existance."
+        },
+         {
+          name: "cclist",
+          value: "Lists all CC commands currently in use."
+        },
+       ],
+      }
+     });
+  }
   if (command === "count") {
     message.channel.send(`There are ${message.guild.memberCount} people in the discord!`);
   }
@@ -1044,114 +1151,7 @@ if (command === "ssk") {
       }    
 });
   }
-  if (command === "hug") {
-    var mention = message.mentions.members.first();
-    if (message.isMentioned(mention)) {
-    message.channel.send(`(>^_^)> ${mention} <(^.^<)`)
-  }
-    else 
-      message.channel.send(`(>^_^)> ${message.author} <(^.^<)`)
-  }
-  if (command === "donate") {
-      message.channel.send("Donate here: https://www.paypal.com/pools/c/839i9RcUvF")
-}
-  if (command === "purge" && !isNaN(args[0])) {
-
-    if (message.member.permissions.has('VIEW_AUDIT_LOG')) {
-    var numb = args[0]
-    message.channel.bulkDelete(numb);
-        message.channel.send(`${numb} messages deleted!`)
-  .then(msg => {
-    msg.delete(5000);
-  })
-  .catch();
-    }
-    else
-      message.channel.send("Nope.");
-  }
- if (command === "support") {
-   message.channel.send("No worries, a staff member will be with you soon!")
-  client.channels.get("442726774909042689").send(`<@&432338355683065856>, ${message.author} is having a bit of a problem in ${message.channel}`);
-  }
-  if (command === "commands") {
-    message.channel.send("You have been messaged the commands!")
-    message.author.send({"embed": {
-    "color": 3947583,
-    "title": "Use prefix '!'",
-        fields: [
-          {
-          name: "scp-[Any number between 1 and 3999](-j opional)",
-          value: "Links to that SCP's page."
-        },
-                 {
-          name: "hug",
-          value: "Gives a hug to you or the person you mention!"
-        },
-                 {
-          name: "donate",
-          value: "Links to the official Kurva PayPal page!"
-        },
-                 {
-          name: "commands",
-          value: "You *just* did it."
-        },
-                 {
-          name: "support",
-          value: "Tells a staff member that you need help. Do not abuse it or there will be consequences."
-        },
-          {
-          name: "kgmeme",
-          value: "Give you a free Kurva Gaming certified meme."
-        },
-         {
-          name: "ss0, 1, 2, 3, k",
-          value: "Tells you the current status of the server."
-        },
-        {
-          name: "8ball",
-          value: "Answers your question *very* truthfully."
-        },
-         {
-          name: "cah",
-          value: "Continues your sentence with a white card from Cards Against Humanity!"
-        },
-                 {
-          name: "purge\nADMIN ONLY",
-          value: "Deletes specified number of messages."
-        },
-         {
-          name: "status\nADMIN ONLY",
-          value: "Sets the **Playing** message for the bot."
-        },
-         {
-          name: "ping\nOWNER ONLY",
-          value: "Pings a person the given number of times.\nUp to 5 reccomended."
-        },
-        {
-          name: "restart\nMOD ONLY",
-          value: "Restarts the bot."
-        },
-         {
-          name: "vote\nADMIN ONLY",
-          value: "Starts a vote!"
-        },
-        {
-          name: "cc (command) (text)\nADMIN ONLY",
-          value: "The bot will say the (text) when you do !(command)."
-        },
-        {
-          name: "ccdel (command)\nADMIN ONLY",
-          value: "The command will be deleted from existance."
-        },
-         {
-          name: "cclist",
-          value: "Lists all CC commands currently in use."
-        },
-       ],
-      }
-     });
-  }
-  }
+   }
 });
 client.on('guildMemberAdd', (member) => {
     member.guild.channels.get('439751531353341954').send(`${member} welcome :)`); 
