3049c6f4c7e3a32d65b94c5cf2a52089b5b5bddd
redassser <40395425+redassser@users.noreply.github.com>
2018-09-02 21:43:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -694,18 +694,11 @@ else if (command === "ss5") {
   var portEnd = "7778"
   var theip = "192.223.27.212";
 }
-               request(`https://api.scpslgame.com/lobbylist.php?format=json`, function(err, resp, html) {
-                 console.log('jj');
+      request(`https://kigen.co/scpsl/getinfo.php?ip=${theip}&port=${portEnd}`, function(err, resp, html) {
         if (!err){
-    {
-              var json = JSON.parse(html);
-     if ("error" in json) {
-     console.log("wtf0");
-     } else {
-       console.log('gg');
-       var obj = json.find(o => o.ip === theip && o.port === portEnd);
-          if(!obj) {
-           message.channel.send({"embed": {
+          var $ = cheerio.load(html); 
+                      if (html === '{"error":"Server not found"}') {
+          message.channel.send({"embed": {
     "color": 9245716,
     timestamp: new Date(),
     "title": `${title}`,
@@ -731,10 +724,16 @@ else if (command === "ss5") {
           ],
       }
      }); 
-          } else {
-            console.log('hh');
-            var playerCount = obj.players
-                      message.channel.send({"embed": {
+            } else {
+              var json = JSON.parse(html);
+     
+     if ("error" in json) {
+     console.log("wtf0");
+     } else {
+          var playerCount = json.players;
+          
+     }
+            message.channel.send({"embed": {
     "color": 3498293,
     timestamp: new Date(),
     "title": `${title}`,
@@ -760,11 +759,9 @@ else if (command === "ss5") {
           ],
       }
      });  
-          }
-     } 
             }
       }    
-});
+ });
  }
   if (command === "ss0"||command === "ssk") {
 if (command === "ss0") {
