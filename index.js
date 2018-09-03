97ee69531c2ec3c127afe1d97f3814474ff49784
redassser <40395425+redassser@users.noreply.github.com>
2018-09-03 17:27:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -500,7 +500,7 @@ for (step = 0; step < args[1]; step++) {
     request('https://api.scpslgame.com/lobbylist.php?format=json', function(err, resp, html) {
       console.log(err)
         if (!err){
-    {
+    console.log('gg')
               var json = JSON.parse(html);
      if ("error" in json) {
      console.log("Someone help me!");
@@ -508,6 +508,7 @@ for (step = 0; step < args[1]; step++) {
       
        var obj = json.find(o => o.ip === "192.223.31.157" && o.port === 7776+i);
           if(!obj) {
+            console.log("offline")
            serverArray.push("Server "+i+" "+"Offline"); 
           } else {
             console.log("Server "+i+" "+obj.players);
