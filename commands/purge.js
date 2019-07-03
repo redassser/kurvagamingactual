9de412bbb0b1f91280a2bac77194fbd5a492c1d5
redassser <40395425+redassser@users.noreply.github.com>
2019-07-03 10:49:50 -0400

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -4,8 +4,8 @@ module.exports = {
         execute(message, args) {
                 if (!message.member.permissions.has('VIEW_AUDIT_LOG')) {message.channel.send("``Senior staff only``");return}
                 if (isNaN(args[0]) || args.length != 1) {message.channel.send("``!purge [number]``");return}
-                msg.bulkDelete(args[0]);
-                msg.send(`${args[0]} messages deleted!`)
+                message.channel.bulkDelete(args[0]);
+                message.channel.send(`${args[0]} messages deleted!`)
                 .then(mesg => {mesg.delete(5000)})
                 .catch();   
         },
