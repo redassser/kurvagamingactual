35a22537a0980769ba6bdedc20bf4b99daf65d2c
redassser <40395425+redassser@users.noreply.github.com>
2019-07-03 10:04:09 -0400

    Create hug.js

---
+++ commands/hug.js
@@ -0,0 +1,9 @@
+ module.exports = {
+	name: 'hug',
+	description: 'Get hugs!',
+	execute(message, args) {
+		var mention = message.mentions.members.first();
+    if (!message.isMentioned(mention)) {msg.send(`(>^_^)> ${message.author} <(^.^<)`);return}
+    msg.send(`(>^_^)> ${mention} <(^.^<)`);
+	},
+};
